asergeant01/sapcc-helm-charts

[openstack/utils] Opt-in for Central Rabbitmq
Each application can opt-in into the central
rabbitmq for notifications by setting the following values.

audit:
  central_service:
    user: <username>
    password: <password>
    name: "hermes-rabbitmq-notifications.hermes"

After the deployment has finished and all messages have
been drained from the old individual rabbitmq,
the charts can be modified to undeploy it

openstack/utils/templates/_ini_sections.tpl
@@ -49,17 +49,19 @@ enabled = true
 {{- define "ini_sections.audit_middleware_notifications"}}
     {{- if .Values.audit }}
         {{- if .Values.audit.enabled }}
-            {{- if .Values.rabbitmq_notifications }}
-                {{- if and .Values.rabbitmq_notifications.ports .Values.rabbitmq_notifications.users }}
 
 # this is for the cadf audit messaging
 [audit_middleware_notifications]
 # topics = notifications
 driver = messagingv2
-transport_url = rabbit://{{ .Values.rabbitmq_notifications.users.default.user }}:{{ required ".Values.rabbitmq_notifications.users.default.password missing" .Values.rabbitmq_notifications.users.default.password | urlquery}}@{{ .Chart.Name }}-rabbitmq-notifications:{{ .Values.rabbitmq_notifications.ports.public }}/
-mem_queue_size = {{ .Values.audit.mem_queue_size }}
+            {{- if .Values.audit.central_service }}
+transport_url = rabbit://{{ .Values.audit.central_service.user | required "Please set audit.central_service.user" }}:{{ .Values.audit.central_service.password | required "Please set audit.central_service.password" | urlquery }}@{{ .Values.audit.central_service.name | required "Please set audit.central_service.name" }}:{{.Values.audit.central_service.port | default 5672 }}/
+            {{- else if .Values.rabbitmq_notifications }}
+                {{- if and .Values.rabbitmq_notifications.ports .Values.rabbitmq_notifications.users }}
+transport_url = rabbit://{{ .Values.rabbitmq_notifications.users.default.user }}:{{ required ".Values.rabbitmq_notifications.users.default.password missing" .Values.rabbitmq_notifications.users.default.password | urlquery }}@{{ .Chart.Name }}-rabbitmq-notifications:{{ .Values.rabbitmq_notifications.ports.public }}/
                 {{- end }}
             {{- end }}
+mem_queue_size = {{ .Values.audit.mem_queue_size }}
         {{- end }}
     {{- end }}
 {{- end }}

rezash84/helm-charts

openstack/utils/templates/_ini_sections.tpl
@@ -51,17 +51,19 @@ enabled = true
 {{- define "ini_sections.audit_middleware_notifications"}}
     {{- if .Values.audit }}
         {{- if .Values.audit.enabled }}
-            {{- if .Values.rabbitmq_notifications }}
-                {{- if and .Values.rabbitmq_notifications.ports .Values.rabbitmq_notifications.users }}
 
 # this is for the cadf audit messaging
 [audit_middleware_notifications]
 # topics = notifications
 driver = messagingv2
-transport_url = rabbit://{{ .Values.rabbitmq_notifications.users.default.user }}:{{ required ".Values.rabbitmq_notifications.users.default.password missing" .Values.rabbitmq_notifications.users.default.password | urlquery}}@{{ .Chart.Name }}-rabbitmq-notifications:{{ .Values.rabbitmq_notifications.ports.public }}/
-mem_queue_size = {{ .Values.audit.mem_queue_size }}
+            {{- if .Values.audit.central_service }}
+transport_url = rabbit://{{ .Values.audit.central_service.user | required "Please set audit.central_service.user" }}:{{ .Values.audit.central_service.password | required "Please set audit.central_service.password" | urlquery }}@{{ .Values.audit.central_service.host | required "Please set audit.central_service.host" }}:{{.Values.audit.central_service.port | default 5672 }}/
+            {{- else if .Values.rabbitmq_notifications }}
+                {{- if and .Values.rabbitmq_notifications.ports .Values.rabbitmq_notifications.users }}
+transport_url = rabbit://{{ .Values.rabbitmq_notifications.users.default.user }}:{{ required ".Values.rabbitmq_notifications.users.default.password missing" .Values.rabbitmq_notifications.users.default.password | urlquery }}@{{ .Chart.Name }}-rabbitmq-notifications:{{ .Values.rabbitmq_notifications.ports.public }}/
                 {{- end }}
             {{- end }}
+mem_queue_size = {{ .Values.audit.mem_queue_size }}
         {{- end }}
     {{- end }}
 {{- end }}